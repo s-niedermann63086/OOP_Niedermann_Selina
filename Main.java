4c5eb280a3dcf417b4dfbd0cd3cee5e6caaa4539
s-niedermann63086 <39619016+s-niedermann63086@users.noreply.github.com>
2020-01-28 09:20:57 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package oop_Niedermann_Selina;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+		
+		public void start(Stage primaryStage) {
+
+			primaryStage.show();
+
+		}
+
+		public static void main(String[] args) {
+			launch(args);
+		}
+	}
+
+
+
